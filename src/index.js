1fbe1e30b6351a4633f5a0a050b1a5f1f8b7f0a0
Vinson 2 <vinsonhe2@gmail.com>
2025-03-14 14:21:07 -0700

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -3,4 +3,30 @@ import homeLoad from "./home.js";
 import menuLoad from "./menu.js";
 import aboutLoad from "./about.js";
 
-aboutLoad();
+const content = document.querySelector('#content');
+const homeButton = document.querySelector('.home-button');
+const menuButton = document.querySelector('.menu-button');
+const aboutButton = document.querySelector('.about-button');
+
+homeButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    content.replaceChildren();
+    homeLoad();
+});
+
+menuButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    content.replaceChildren();
+    menuLoad();
+});
+
+aboutButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    content.replaceChildren();
+    aboutLoad();
+})
+
+homeLoad();
